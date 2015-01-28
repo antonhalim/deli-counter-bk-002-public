4351fe7bf7ca9c57044ab545f8fd6117ea62a4db
axton21 <axton_n@yahoo.com>
2015-01-28 18:16:39 -0500

    labs done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def take_a_number(deli, patron)
+	deli.push(patron)
+	spot = deli.length
+	puts "Welcome, #{patron}. You are number #{spot} in line."
+	end
+
+def line(deli)
+	if deli.empty?
+		puts "The line is currently empty."
+	else
+		queue = deli.map.with_index(1){|patron, spot| "#{spot}. #{patron}"}
+			puts "The line is currently: #{queue.join(" ")}"
+		end
+	end
+
+def now_serving(deli)
+	if deli.empty?
+		puts "There is nobody waiting to be served!"
+      else
+      	puts "Currently serving #{deli.shift}."
+      end
+  end
